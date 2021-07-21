dc449fe3f4f682d648e44c1ed4130f2c5a758c7d
Sergio <colombia00028@gmail.com>
2021-07-20 19:34:49 -0500

    Design Dog in Main

--- src/tamagotchi_dog/Tamagotchi_Dog.java
+++ src/tamagotchi_dog/Tamagotchi_Dog.java
@@ -4,6 +4,19 @@ public class Tamagotchi_Dog {
 
     public static void main(String[] args) {
        
+       System.out.println("Start Game!");     
+       Dog Scrappy = new Dog("Pandy","Blue", 3,4);
+       System.out.println(Scrappy.getColour());
+       System.out.println(Scrappy.getEnergy());
+       System.out.println(Scrappy.getEatLevel());
+       Scrappy.bark();
+       for(int i =0; i<=2; i++){
+       Scrappy.play();
     }
+       Scrappy.setEnergy(4);
+       Scrappy.comer(3);
+       Scrappy.play();
+    
+}
     
 }
